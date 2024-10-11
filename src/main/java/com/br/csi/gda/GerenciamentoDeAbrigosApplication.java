8e2aee49f0862d84418170b1160012e944952a9b
Andre <142367391+AndreSiF@users.noreply.github.com>
2024-10-11 18:15:49 -0300

    Spring Boot Initializer
    Inserção dos arquivos do initializer.

---
+++ src/main/java/com/br/csi/gda/GerenciamentoDeAbrigosApplication.java
@@ -0,0 +1,13 @@
+package com.br.csi.gda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeAbrigosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeAbrigosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/csi/gda/GerenciamentoDeAbrigosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.csi.gda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeAbrigosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
