c4cda8c99337c5bebedce2207b73ac4b159e6bc6
Andre <142367391+AndreSiF@users.noreply.github.com>
2024-10-15 16:36:57 -0300

    deleting useless files
    removendo os arquivos inúteis do projeto antigo.

--- src/main/java/com/br/csi/gda/test/test.java
+++ src/main/java/com/br/csi/gda/test/test.java
@@ -1,6 +1,5 @@
 package com.br.csi.gda.test;
 
-import com.br.csi.gda.dao.UsuarioDAO;
 import com.br.csi.gda.model.Permissao;
 import com.br.csi.gda.model.Usuario;
 
@@ -8,6 +7,5 @@ public class test {
     public static void main(String[] args) {
         Permissao permissao = new Permissao(3, "VOLUNTARIO");
         Usuario usuario = new Usuario("Kaue", "173.486.789-10", 43, permissao);
-        new UsuarioDAO().cadastrar(usuario);
     }
 }
